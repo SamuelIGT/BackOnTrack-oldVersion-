737187ab2e96b9a2265707e00e25687c75b9d81d
SamuelIGT <samuel.br.igt@gmail.com>
2017-07-17 18:28:27 -0300

    Back on Track v0.03
    RecyclerView  fully working.

--- app/src/main/java/br/ufc/quixada/backontrack/MainActivity.java
+++ app/src/main/java/br/ufc/quixada/backontrack/MainActivity.java
@@ -1,25 +1,17 @@
 package br.ufc.quixada.backontrack;
 
-import android.app.ActionBar;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.PagerAdapter;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -119,5 +111,4 @@ public class MainActivity extends AppCompatActivity {
             return fragment;
         }
     }
-
 }
