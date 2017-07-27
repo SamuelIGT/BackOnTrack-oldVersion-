0a7c0a105af1bd5247228405492f55c2ef89c37c
SamuelIGT <samuel.br.igt@gmail.com>
2017-07-27 20:00:53 -0300

    Back On Track v0.5.5-ALPHA
    *FIXED: Card alignment not centralized.
    *IMPROVED: UI layout.

--- app/src/main/java/br/ufc/quixada/backontrack/activities/ExerciseActivity.java
+++ app/src/main/java/br/ufc/quixada/backontrack/activities/ExerciseActivity.java
@@ -1,5 +1,6 @@
 package br.ufc.quixada.backontrack.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Build;
@@ -15,6 +16,7 @@ import android.support.v7.widget.RecyclerView;
 
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -42,38 +44,43 @@ public class ExerciseActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.fragment_main);
-        //Initializes the collapsing toolbar
-        initCollapsingToolbar();
-
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
         //gets the section that was chosen
         sec = (Section) getIntent().getSerializableExtra(getString(R.string.sectionToExercise_extraTAG));
-        adapter = new ExerciseAdapter(this, sec.getExerciseList());
 
+        //Initializes the collapsing toolbar
+        initCollapsingToolbar(this);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        adapter = new ExerciseAdapter(this, sec.getExerciseList());
+        prepareAtivity();
 
         RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
         recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        //recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true)); //Defines how the cards will be shown
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(adapter);
 
-        //
 
-        prepareAtivity();
 
 
 
     }
 
-    private void initCollapsingToolbar(){
+    private void initCollapsingToolbar(final Context context){
         final CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbar.setTitle(" ");
+
+        collapsingToolbar.setTitle(sec.getTitle());
+        collapsingToolbar.setExpandedTitleColor(ContextCompat.getColor(context,R.color.primary_text));
+
         AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
         appBarLayout.setExpanded(true);
+
+
+
         // hiding & showing the title when toolbar expanded & collapsed
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener(){
             boolean isShow = false;
@@ -83,21 +90,26 @@ public class ExerciseActivity extends AppCompatActivity {
                 if(scroolRange == -1){
                     scroolRange = appBarLayout.getTotalScrollRange();
                 }
+                //if its collapsed
                 if(scroolRange + verticalOffset == 0){
+
                     collapsingToolbar.setTitle(sec.getTitle());
                     isShow = true;
+
+                    //if its expanded
                 } else if(isShow){
-                    collapsingToolbar.setTitle(" ");
+
+                    collapsingToolbar.setTitle(sec.getTitle());
                     isShow = false;
                 }
             }
         });
     }
 
-    private int dpToPx(int dp) {
+/*    private int dpToPx(int dp) {
         Resources r = getResources();
         return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
-    }
+    }*/
 
     //Just for testing purpose
     private void prepareAtivity(){
