3bfb6b2037cb793aad2c0984a0a1643293340b58
SamuelIGT <samuel.br.igt@gmail.com>
2017-07-29 20:15:11 -0300

    Back On Track v0.7.6
    *Progressbar now uses SmoothProgressBar, a new API integrated with the
    system.

--- app/src/main/java/br/ufc/quixada/backontrack/activities/ExerciseExecutionActivity.java
+++ app/src/main/java/br/ufc/quixada/backontrack/activities/ExerciseExecutionActivity.java
@@ -1,18 +1,22 @@
 package br.ufc.quixada.backontrack.activities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import br.ufc.quixada.backontrack.R;
+import br.ufc.quixada.backontrack.progressBar.ProgressBarAnimation;
 
 /**
  * Created by samue on 28/07/2017.
@@ -24,6 +28,8 @@ public class ExerciseExecutionActivity extends AppCompatActivity {
     private TextView timer;
     private TextView txtViewSteps;
     private Button startStop;
+    private ProgressBar pgBar;
+    private ProgressBar pgBarFilled;
 
     int min, s, ms;
     private boolean isRunning;
@@ -37,10 +43,18 @@ public class ExerciseExecutionActivity extends AppCompatActivity {
         txtViewSteps = (TextView) findViewById(R.id.txt_steps);
         timer = (TextView) findViewById(R.id.timer);
         startStop = (Button) findViewById(R.id.start_stop);
+       // pgBar = (ProgressBar) findViewById(R.id.pgBar);
+       // pgBarFilled = (ProgressBar) findViewById(R.id.pgBar_filled);
 
+
+
+ /*       ProgressBarAnimation anim = new ProgressBarAnimation(pgBar, 0, 100);
+        anim.setDuration(1000);
+        pgBar.startAnimation(anim);
+*/
         setupSteps();
 
-        setupTimer();
+        setupTimer(this);
 
     }
 
@@ -55,17 +69,22 @@ public class ExerciseExecutionActivity extends AppCompatActivity {
         }
     }
 
-    private void setupTimer() {
+    private void setupTimer(final Context context) {
 
 
         startStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!isRunning) {
+                    //pgBar.setVisibility(View.VISIBLE);
+                    //pgBarFilled.setVisibility(View.INVISIBLE);
+
                     startTime = SystemClock.uptimeMillis();
                     h.postDelayed(r, 0);
                     isRunning = true;
                 } else {
+                   // pgBar.setVisibility(View.INVISIBLE);
+                  //  pgBarFilled.setVisibility(View.VISIBLE);
                     timeSB += timeMS;
                     h.removeCallbacks(r);
                     isRunning = false;
@@ -97,8 +116,8 @@ public class ExerciseExecutionActivity extends AppCompatActivity {
             s = (int) (update / 1000);
             min = s / 60;
             s = s % 60;
-            ms = (int) update % 1000;
-            timer.setText("" + String.format("%02d", min) + ":" + String.format("%02d", s) + ":" + String.format("%01d", ms));
+            ms = (int)(((int)update % 1000) / 10);
+            timer.setText("" + String.format("%02d", min) + ":" + String.format("%02d", s) + ":" + ms);
             h.postDelayed(this, 0);
         }
     };
