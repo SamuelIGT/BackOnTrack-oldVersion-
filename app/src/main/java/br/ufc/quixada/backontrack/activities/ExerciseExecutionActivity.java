44131bb96e1151449f610a26f94ff6692791c11f
SamuelIGT <samuel.br.igt@gmail.com>
2017-07-30 16:43:47 -0300

    Back on Track v0.7.7-ALPHA
    *Chronometer layout improved

--- app/src/main/java/br/ufc/quixada/backontrack/activities/ExerciseExecutionActivity.java
+++ app/src/main/java/br/ufc/quixada/backontrack/activities/ExerciseExecutionActivity.java
@@ -8,7 +8,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -16,7 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.ufc.quixada.backontrack.R;
-import br.ufc.quixada.backontrack.progressBar.ProgressBarAnimation;
+import fr.castorflex.android.circularprogressbar.CircularProgressBar;
+import fr.castorflex.android.circularprogressbar.CircularProgressDrawable;
 
 /**
  * Created by samue on 28/07/2017.
@@ -24,12 +25,13 @@ import br.ufc.quixada.backontrack.progressBar.ProgressBarAnimation;
 
 public class ExerciseExecutionActivity extends AppCompatActivity {
 
-    private List<String> steps;
+    //private List<String> steps;
     private TextView timer;
     private TextView txtViewSteps;
     private Button startStop;
-    private ProgressBar pgBar;
-    private ProgressBar pgBarFilled;
+    private CircularProgressBar pgBar;
+    private ImageView staticPgBar;
+    private CircularProgressDrawable pgBarDrawable;
 
     int min, s, ms;
     private boolean isRunning;
@@ -40,25 +42,20 @@ public class ExerciseExecutionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exercise_execution);
 
-        txtViewSteps = (TextView) findViewById(R.id.txt_steps);
+       // txtViewSteps = (TextView) findViewById(R.id.txt_steps);
         timer = (TextView) findViewById(R.id.timer);
         startStop = (Button) findViewById(R.id.start_stop);
-       // pgBar = (ProgressBar) findViewById(R.id.pgBar);
-       // pgBarFilled = (ProgressBar) findViewById(R.id.pgBar_filled);
+        pgBar = (CircularProgressBar) findViewById(R.id.pgBar);
+        staticPgBar = (ImageView) findViewById(R.id.pgBarStatic);
+        pgBarDrawable = (CircularProgressDrawable) pgBar.getIndeterminateDrawable();
 
-
-
- /*       ProgressBarAnimation anim = new ProgressBarAnimation(pgBar, 0, 100);
-        anim.setDuration(1000);
-        pgBar.startAnimation(anim);
-*/
-        setupSteps();
+//        setupSteps();
 
         setupTimer(this);
 
     }
 
-    private void setupSteps() {
+/*    private void setupSteps() {
         steps = new ArrayList<String>();
         steps.add("Sente-se em uma cadeira.");
         steps.add("Coloque suas mãos na mesa.");
@@ -67,24 +64,29 @@ public class ExerciseExecutionActivity extends AppCompatActivity {
         for (String step : steps) {
             txtViewSteps.setText(txtViewSteps.getText() + step + "\n");
         }
-    }
+    }*/
 
     private void setupTimer(final Context context) {
-
+        pgBar.setVisibility(View.INVISIBLE);
 
         startStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!isRunning) {
-                    //pgBar.setVisibility(View.VISIBLE);
-                    //pgBarFilled.setVisibility(View.INVISIBLE);
+                    startStop.setText(R.string.button_stop);
+//                    staticPgBar.setVisibility(View.INVISIBLE);
+                    pgBar.setVisibility(View.VISIBLE);
+                    pgBarDrawable.start();
 
                     startTime = SystemClock.uptimeMillis();
                     h.postDelayed(r, 0);
                     isRunning = true;
                 } else {
-                   // pgBar.setVisibility(View.INVISIBLE);
-                  //  pgBarFilled.setVisibility(View.VISIBLE);
+                    startStop.setText(R.string.button_start);
+//                    staticPgBar.setVisibility(View.VISIBLE);
+                    pgBar.setVisibility(View.INVISIBLE);
+                    pgBarDrawable.stop();
+
                     timeSB += timeMS;
                     h.removeCallbacks(r);
                     isRunning = false;
@@ -117,7 +119,7 @@ public class ExerciseExecutionActivity extends AppCompatActivity {
             min = s / 60;
             s = s % 60;
             ms = (int)(((int)update % 1000) / 10);
-            timer.setText("" + String.format("%02d", min) + ":" + String.format("%02d", s) + ":" + ms);
+            timer.setText("" + String.format("%02d", min) + ":" + String.format("%02d", s) + ":" + String.format("%02d", ms));
             h.postDelayed(this, 0);
         }
     };
