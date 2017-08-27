f1133eeda446b4e3336d48c0a489de0815a51513
SamuelIGT <samuel.br.igt@gmail.com>
2017-08-27 18:53:06 -0300

    Back on Track v0.13.19-ALPHA
    Fixed some problems when shows the events of the day on Calendar screen.

---
+++ app/src/main/java/br/ufc/quixada/backontrack/ExerciseReport.java
@@ -0,0 +1,62 @@
+package br.ufc.quixada.backontrack;
+
+/**
+ * Created by samuel on 25/08/17.
+ */
+
+public class ExerciseRelatory {
+    private int exerciseId;
+    private String exerciseTitle;
+    private int effort;
+    private long dateMillis;
+    private String time;
+
+    public ExerciseRelatory(int exerciseId, String exerciseTitle, int effort, long dateMillis, String time) {
+        this.exerciseId = exerciseId;
+        this.exerciseTitle = exerciseTitle;
+        this.effort = effort;
+        this.dateMillis = dateMillis;
+        this.time = time;
+    }
+
+    public ExerciseRelatory(int exerciseId) {
+        this.exerciseId = exerciseId;
+    }
+
+
+    public int getExerciseId() {
+        return exerciseId;
+    }
+
+    public String getExerciseTitle() {
+        return exerciseTitle;
+    }
+
+    public int getEffort() {
+        return effort;
+    }
+
+    public long getDateMillis() {
+        return dateMillis;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setExerciseTitle(String exerciseTitle) {
+        this.exerciseTitle = exerciseTitle;
+    }
+
+    public void setEffort(int effort) {
+        this.effort = effort;
+    }
+
+    public void setDateMillis(long dateMillis) {
+        this.dateMillis = dateMillis;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
